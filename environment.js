232e68abf793b5e6736145e73f07ddab1aab8436
juaznerzz <juaznerzzcop@gmail.com>
2025-06-16 19:39:29 -0500

    Update environment.js

--- environment.js
+++ environment.js
@@ -1,5 +1,5 @@
 const whatsappNumber = "573195999136";
-localStorage.setItem("URL", `https://api.sincodigo.pro`);
+localStorage.setItem("URL", `https://puente-1.onrender.com`);
 localStorage.setItem(
   "URL_REDIRECT",
   `https://wa.me/${whatsappNumber}&text=Hola, desinstalé la extension por error`
